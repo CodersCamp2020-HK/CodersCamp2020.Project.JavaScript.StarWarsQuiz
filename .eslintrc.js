f692f9826e089776040667fc0fc149677d3421fa
Hubert Kawałek <hubert.kawalek.dev@gmail.com>
2020-12-19 02:19:56 +0100

    Set up eslint along with prettier and add support for vscode

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    env: {
+        es6: true,
+        node: true,
+        browser: true,
+        jest: true,
+    },
+    extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+    },
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+    },
+    rules: {
+        'no-console': 'off',
+    },
+};
--- src/app/App.js
+++ src/app/App.js
@@ -1,1 +1,1 @@
-export const App = ({ options }) => {};
+export const App = () => {};
