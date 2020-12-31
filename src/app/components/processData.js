e691ec09f70716e2ab01a51508cdd6ac2ba3ebfb
Mateusz Król <matek8952@gmail.com>
2020-12-31 18:08:50 +0100

    Preprocessing record from People working for name and index.

--- src/app/components/processData.js
+++ src/app/components/processData.js
@@ -2,10 +2,14 @@ import { Data } from './fetchApi';
 
 class PrepareData {
     static async preprocessPeople() {
+        const regexForEndpoint = /\d+\/$/;
         const peopleJsonData = await Data.getPeopleJsonData();
         for (const charactersArray of peopleJsonData) {
             for (const character of charactersArray.results) {
-                console.log(character);
+                const name = character.name;
+                const indexEndpoint = character.url.match(regexForEndpoint)[0].toString();
+                const index = indexEndpoint.slice(0, -1);
+                console.log(name, index);
             }
         }
     }
