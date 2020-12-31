fac750d26e15b47f8cce7ff7801b7fd4e4e2170b
Mateusz Król <matek8952@gmail.com>
2020-12-31 16:44:57 +0100

    Static function for fetching people from API as a Promise

--- src/app/components/fetchApi.js
+++ src/app/components/fetchApi.js
@@ -1,8 +1,21 @@
 class Data {
-    getPeopleData() {
-        fetch('https://swapi.dev/api/people/')
-            .then((response) => response.json())
-            .then((data) => console.log(data));
+    static async getPeopleData() {
+        const peopleEndpoints = ['https://swapi.dev/api/people/'];
+        for (let i = 1; i < 10; i++) {
+            peopleEndpoints.push(`https://swapi.dev/api/people/?page=${i}`);
+        }
+        return await Promise.all([
+            fetch(peopleEndpoints[0]),
+            fetch(peopleEndpoints[1]),
+            fetch(peopleEndpoints[2]),
+            fetch(peopleEndpoints[3]),
+            fetch(peopleEndpoints[4]),
+            fetch(peopleEndpoints[5]),
+            fetch(peopleEndpoints[6]),
+            fetch(peopleEndpoints[7]),
+            fetch(peopleEndpoints[8]),
+            fetch(peopleEndpoints[9]),
+        ]);
     }
 
     getVehiclesData() {
