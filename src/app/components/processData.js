37ff3b118fbf402a99e8fdf7370a08a5ec158b43
Mateusz Król <matek8952@gmail.com>
2020-12-31 18:15:01 +0100

    Preprocessed data for question about characters

--- src/app/components/processData.js
+++ src/app/components/processData.js
@@ -4,14 +4,17 @@ class PrepareData {
     static async preprocessPeople() {
         const regexForEndpoint = /\d+\/$/;
         const peopleJsonData = await Data.getPeopleJsonData();
+        const peopleDataQuestions = [];
+
         for (const charactersArray of peopleJsonData) {
             for (const character of charactersArray.results) {
                 const name = character.name;
                 const indexEndpoint = character.url.match(regexForEndpoint)[0].toString();
                 const index = indexEndpoint.slice(0, -1);
-                console.log(name, index);
+                peopleDataQuestions.push({ name, index });
             }
         }
+        return peopleDataQuestions;
     }
 }
 
