c3797bce986615e9edbdef8565a7119190f3a4de
Mateusz Król <matek8952@gmail.com>
2020-12-31 18:16:57 +0100

    Adding App.js for usage of functions

--- src/app/App.js
+++ src/app/App.js
@@ -1,1 +1,15 @@
-export const App = () => {};
+import { Data } from './components/fetchApi';
+import { PrepareData } from './components/processData';
+
+export const App = () => {
+    const people = Data.getPeopleJsonData();
+    people.then((response) => console.log(response));
+
+    const vehicles = Data.getVehiclesJsonData();
+    vehicles.then((response) => console.log(response));
+
+    const starships = Data.getStarshipsJsonData();
+    starships.then((response) => console.log(response));
+
+    PrepareData.preprocessPeople().then((data) => console.log(data));
+};
