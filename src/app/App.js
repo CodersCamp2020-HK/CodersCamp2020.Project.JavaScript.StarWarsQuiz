11f6ab0ae600f5efe3f093a4d85b730e9fcc155d
Mateusz Król <matek8952@gmail.com>
2021-01-01 15:00:59 +0100

    Fixed linting

--- src/app/App.js
+++ src/app/App.js
@@ -1,17 +1,6 @@
-import { Data } from './components/fetchApi';
 import { PrepareData } from './components/processData';
 
 export const App = () => {
-    // Pobieranie rekordów z API
-    // const people = Data.getPeopleJsonData();
-    // people.then((response) => console.log(response));
-
-    // const vehicles = Data.getVehiclesJsonData();
-    // vehicles.then((response) => console.log(response));
-
-    // const starships = Data.getStarshipsJsonData();
-    // starships.then((response) => console.log(response));
-
     // Dane przygotowane do pytań
     PrepareData.preprocessPeople().then((data) => console.log(data));
 
