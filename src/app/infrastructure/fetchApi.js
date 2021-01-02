1b3c97fad512d1e691443a8dcd9fbac245b79147
Mateusz Król <matek8952@gmail.com>
2021-01-02 18:24:35 +0100

    fetchApi refactor

--- src/app/infrastructure/fetchApi.js
+++ src/app/infrastructure/fetchApi.js
@@ -2,35 +2,15 @@ import { peopleEndpoints, vehiclesEndpoints, starshipsEndpoints } from './endpoi
 
 class Data {
     static async getPeopleJsonData() {
-        return await Promise.all([
-            fetch(peopleEndpoints[0]).then((response) => response.json()),
-            fetch(peopleEndpoints[1]).then((response) => response.json()),
-            fetch(peopleEndpoints[2]).then((response) => response.json()),
-            fetch(peopleEndpoints[3]).then((response) => response.json()),
-            fetch(peopleEndpoints[4]).then((response) => response.json()),
-            fetch(peopleEndpoints[5]).then((response) => response.json()),
-            fetch(peopleEndpoints[6]).then((response) => response.json()),
-            fetch(peopleEndpoints[7]).then((response) => response.json()),
-            fetch(peopleEndpoints[8]).then((response) => response.json()),
-        ]);
+        return await Promise.all(peopleEndpoints.map((url) => fetch(url).then((resp) => resp.json())));
     }
 
     static async getVehiclesJsonData() {
-        return await Promise.all([
-            fetch(vehiclesEndpoints[0]).then((response) => response.json()),
-            fetch(vehiclesEndpoints[1]).then((response) => response.json()),
-            fetch(vehiclesEndpoints[2]).then((response) => response.json()),
-            fetch(vehiclesEndpoints[3]).then((response) => response.json()),
-        ]);
+        return await Promise.all(vehiclesEndpoints.map((url) => fetch(url).then((resp) => resp.json())));
     }
 
     static async getStarshipsJsonData() {
-        return await Promise.all([
-            fetch(starshipsEndpoints[0]).then((response) => response.json()),
-            fetch(starshipsEndpoints[1]).then((response) => response.json()),
-            fetch(starshipsEndpoints[2]).then((response) => response.json()),
-            fetch(starshipsEndpoints[3]).then((response) => response.json()),
-        ]);
+        return await Promise.all(starshipsEndpoints.map((url) => fetch(url).then((resp) => resp.json())));
     }
 }
 
