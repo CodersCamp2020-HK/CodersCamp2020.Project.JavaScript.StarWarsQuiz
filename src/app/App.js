21256c8b733c896696726ba306615f7ddf24edfa
Mateusz Król <matek8952@gmail.com>
2021-01-02 16:55:16 +0100

    processData rewritten

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,15 @@
+import { Data } from '../app/components/fetchApi';
 import { PrepareData } from './components/processData';
 
 export const App = () => {
     // Dane przygotowane do pytań
-    PrepareData.preprocessPeople().then((data) => console.log(data));
-
-    PrepareData.preprocessVehicles().then((data) => console.log(data));
-
-    PrepareData.preprocessStarships().then((data) => console.log(data));
+    Data.getPeopleJsonData()
+        .then((data) => PrepareData.preprocessData(data))
+        .then((questions) => console.log(questions));
+    Data.getVehiclesJsonData()
+        .then((data) => PrepareData.preprocessData(data))
+        .then((questions) => console.log(questions));
+    Data.getStarshipsJsonData()
+        .then((data) => PrepareData.preprocessData(data))
+        .then((questions) => console.log(questions));
 };
--- src/app/components/processData.js
+++ src/app/components/processData.js
@@ -1,53 +1,19 @@
-import { Data } from './fetchApi';
 import { Question } from './question';
 
 class PrepareData {
-    static async preprocessPeople() {
+    static async preprocessData(jsonData) {
         const regexForEndpoint = /\d+\/$/;
-        const peopleJsonData = await Data.getPeopleJsonData();
-        const peopleDataQuestions = [];
+        const dataQuestions = [];
 
-        for (const charactersArray of peopleJsonData) {
-            for (const character of charactersArray.results) {
-                const name = character.name;
-                const indexEndpoint = character.url.match(regexForEndpoint)[0].toString();
+        for (const objectsArray of jsonData) {
+            for (const singleObject of objectsArray.results) {
+                const name = singleObject.name;
+                const indexEndpoint = singleObject.url.match(regexForEndpoint)[0].toString();
                 const index = indexEndpoint.slice(0, -1);
-                peopleDataQuestions.push(new Question(name, index));
+                dataQuestions.push(new Question(name, index));
             }
         }
-        return peopleDataQuestions;
-    }
-
-    static async preprocessVehicles() {
-        const regexForEndpoint = /\d+\/$/;
-        const vehiclesJsonData = await Data.getVehiclesJsonData();
-        const vehiclesDataQuestions = [];
-
-        for (const vehiclesArray of vehiclesJsonData) {
-            for (const vehicle of vehiclesArray.results) {
-                const name = vehicle.name;
-                const indexEndpoint = vehicle.url.match(regexForEndpoint)[0].toString();
-                const index = indexEndpoint.slice(0, -1);
-                vehiclesDataQuestions.push(new Question(name, index));
-            }
-        }
-        return vehiclesDataQuestions;
-    }
-
-    static async preprocessStarships() {
-        const regexForEndpoint = /\d+\/$/;
-        const starshipsJsonData = await Data.getStarshipsJsonData();
-        const starshipsDataQuestions = [];
-
-        for (const starshipsArray of starshipsJsonData) {
-            for (const starship of starshipsArray.results) {
-                const name = starship.name;
-                const indexEndpoint = starship.url.match(regexForEndpoint)[0].toString();
-                const index = indexEndpoint.slice(0, -1);
-                starshipsDataQuestions.push(new Question(name, index));
-            }
-        }
-        return starshipsDataQuestions;
+        return dataQuestions;
     }
 }
 
