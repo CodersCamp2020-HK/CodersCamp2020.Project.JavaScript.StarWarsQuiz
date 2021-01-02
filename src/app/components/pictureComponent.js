5501a4580923cb90df09fdbe0cf7a5302b754206
Ania-Em <aniaarchi@gmail.com>
2021-01-02 21:20:54 +0100

    Name of category changed

--- src/app/components/pictureComponent.js
+++ src/app/components/pictureComponent.js
@@ -1,11 +1,11 @@
 import {  } from "";
 
-const generatePicture = (category, pictureNumber) =>{
-   const category= "people";
+const generatePicture = (Questioncategory, pictureNumber) =>{
+   const Questioncategory= "people";
    const pictureNumber= 1;
 
    const picture = document.createElement("img");
-   img.src=`./src/static/assets/img/modes/${category}/${pictureNumber}.jpg`;
+   img.src=`./src/static/assets/img/modes/${Questioncategory}/${pictureNumber}.jpg`;
 }
 
 export{generatePicture};
