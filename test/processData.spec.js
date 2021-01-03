6639dd52f66e29054af403b1e07db61286eb04ae
Mateusz Król <matek8952@gmail.com>
2021-01-03 12:06:06 +0100

    Tests

--- test/processData.spec.js
+++ test/processData.spec.js
@@ -38,4 +38,66 @@ describe('Fetching data from API', () => {
             expect(result).toStrictEqual(expected);
         });
     });
+    describe('check if vehicles data', () => {
+        it('preprocess properly', () => {
+            const sampleVehicle1 = {
+                name: 'Sand Crawler',
+                url: 'http://swapi.dev/api/vehicles/4/',
+            };
+            const sampleVehicle2 = {
+                name: 'Storm IV Twin-Pod cloud car',
+                url: 'http://swapi.dev/api/vehicles/20/',
+            };
+            const sampleVehicle3 = {
+                name: 'AT-AT',
+                url: 'http://swapi.dev/api/vehicles/18/',
+            };
+            const sampleResults1 = {
+                results: [sampleVehicle1, sampleVehicle2],
+            };
+            const sampleResults2 = {
+                results: [sampleVehicle3],
+            };
+            const sampleVehiclesData = [sampleResults1, sampleResults2];
+            const expected = [
+                new Question('Sand Crawler', '4'),
+                new Question('Storm IV Twin-Pod cloud car', '20'),
+                new Question('AT-AT', '18'),
+            ];
+            const preprocess = new PrepareData();
+            const result = preprocess.preprocessData(sampleVehiclesData);
+            expect(result).toStrictEqual(expected);
+        });
+    });
+    describe('check if starships data', () => {
+        it('preprocess properly', () => {
+            const sampleStarship1 = {
+                name: 'Star Destroyer',
+                url: 'http://swapi.dev/api/starships/3/',
+            };
+            const sampleStarship2 = {
+                name: 'Death Star',
+                url: 'http://swapi.dev/api/starships/9/',
+            };
+            const sampleStarship3 = {
+                name: 'Millennium Falcon',
+                url: 'http://swapi.dev/api/starships/10/',
+            };
+            const sampleResults1 = {
+                results: [sampleStarship1, sampleStarship2],
+            };
+            const sampleResults2 = {
+                results: [sampleStarship3],
+            };
+            const sampleStarshipsData = [sampleResults1, sampleResults2];
+            const expected = [
+                new Question('Star Destroyer', '3'),
+                new Question('Death Star', '9'),
+                new Question('Millennium Falcon', '10'),
+            ];
+            const preprocess = new PrepareData();
+            const result = preprocess.preprocessData(sampleStarshipsData);
+            expect(result).toStrictEqual(expected);
+        });
+    });
 });
