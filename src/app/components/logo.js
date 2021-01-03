af32db0fbf1f7d5ffe37c9ae4a57d6c3c29d2ffd
Ania-Em <aniaarchi@gmail.com>
2021-01-03 11:54:29 +0100

    Test

--- src/app/components/logo.js
+++ src/app/components/logo.js
@@ -1,6 +1,6 @@
-const logopicture => {
+const logoPicture => {
    const logo = document.createElement("img");
    logo.src='./src/static/assets/ui/StarWarsLogo.png';
 }
 
-export {logopicture};
+export {logoPicture};
