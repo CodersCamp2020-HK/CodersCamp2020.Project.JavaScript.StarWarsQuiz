07c45bc188e769bba73ae380237bcca12a4d84bb
Ania-Em <aniaarchi@gmail.com>
2021-01-03 23:56:13 +0100

    Logo function

--- src/app/components/logo.js
+++ src/app/components/logo.js
@@ -1,6 +1,8 @@
-const logoPicture => {
-   const logo = document.createElement("img");
-   logo.src='./src/static/assets/ui/StarWarsLogo.png';
-}
+const LogoPicture = function () {
+    const logoContainer = document.createElement('img');
+    logoContainer.src = './src/static/assets/ui/StarWarsLogo.png';
+    logoContainer.className = 'Logo';
+    return logoContainer;
+};
 
-export {logoPicture};
+export { LogoPicture };
