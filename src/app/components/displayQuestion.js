4d007e97314fbfc720afa7093cb7680d8f9beda0
Mateusz Król <matek8952@gmail.com>
2021-01-03 20:57:23 +0100

    Working component for displaying question

---
+++ src/app/components/displayQuestion.js
@@ -0,0 +1,35 @@
+export class DisplayQuestion {
+    constructor() {
+        this.questionNumber = 1;
+        this.questionWrapper;
+        this.questionParagraph;
+    }
+
+    generateQuestion(category) {
+        this.questionWrapper = document.createElement('div');
+        this.questionWrapper.className = 'display-question-wrapper';
+
+        this.questionParagraph = document.createElement('p');
+        this.questionParagraph.className = 'display-question-text';
+
+        switch (category) {
+            case 'people':
+                this.questionParagraph.textContent = `${this.questionNumber}. Who is this character?`;
+                break;
+            case 'vehicles':
+                this.questionParagraph.textContent = `${this.questionNumber}. What is this vehicle?`;
+                break;
+            case 'starshipts':
+                this.questionParagraph.textContent = `${this.questionNumber}. What is this starship?`;
+                break;
+            default:
+                this.questionParagraph.textContent = `${this.questionNumber}. Who/What is this character/vehicle/starship?`;
+                break;
+        }
+
+        this.questionWrapper.appendChild(this.questionParagraph);
+        this.questionNumber++;
+
+        return this.questionWrapper;
+    }
+}
