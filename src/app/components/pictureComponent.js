fbca2770b7e4461b8430e4172d9152571f32a309
Ania-Em <aniaarchi@gmail.com>
2021-01-03 11:21:17 +0100

    Fixed

--- src/app/components/pictureComponent.js
+++ src/app/components/pictureComponent.js
@@ -1,8 +1,8 @@
-import {  } from "";
+import {} from '';
 
-const generatePicture = (questionCategory="people", pictureNumber=1) =>{
-   const picture = document.createElement("img");
-   picture.src=`./src/static/assets/img/modes/${questionCategory}/${pictureNumber}.jpg`;
-}
+const generatePicture = (questionCategory = 'people', pictureNumber = 1) => {
+    const picture = document.createElement('img');
+    picture.src = `./src/static/assets/img/modes/${questionCategory}/${pictureNumber}.jpg`;
+};
 
-export{generatePicture};
+export { generatePicture };
