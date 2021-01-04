2f25f73f195123d55d219a41095e94b460bd2b0a
Tomasz Dudek <t.dudeek@gmail.com>
2021-01-04 11:12:19 +0100

    Timeout 9x typed by hand

--- src/app/components/deathStar.js
+++ src/app/components/deathStar.js
@@ -8,8 +8,38 @@ export const deathStar = () => {
     deathStarDiv.appendChild(image);
     document.body.appendChild(deathStarDiv);
     setTimeout(function () {
-        deathStarDiv.style.display = 'none';
-    }, 5000);
+        deathStarDiv.style.clipPath = 'polygon(50% 0, 50% 50%, 100% 0, 100% 100%, 0 100%, 0 0)';
+    }, 1 * 1000);
+    setTimeout(function () {
+        deathStarDiv.style.clipPath = 'polygon(50% 0%, 50% 50%, 100% 50%, 100% 100%, 0 100%, 0 0)';
+    }, 2 * 1000);
 
+    setTimeout(function () {
+        deathStarDiv.style.clipPath = 'polygon(50% 0%, 50% 50%, 100% 100%, 0 100%, 0 100%, 0 0)';
+    }, 3 * 1000);
+
+    setTimeout(function () {
+        deathStarDiv.style.clipPath = 'polygon(50% 0%, 50% 50%, 50% 100%, 0 100%, 0 100%, 0 0)';
+    }, 4 * 1000);
+
+    setTimeout(function () {
+        deathStarDiv.style.clipPath = 'polygon(50% 0%, 50% 50%, 0 100%, 50% 100%, 0 100%, 0 0)';
+    }, 5 * 1000);
+
+    setTimeout(function () {
+        deathStarDiv.style.clipPath = 'polygon(50% 0, 50% 50%, 0 100%, 50% 100%, 0 100%, 0 0)';
+    }, 6 * 1000);
+
+    setTimeout(function () {
+        deathStarDiv.style.clipPath = 'polygon(50% 0, 50% 50%, 0 50%, 0 50%, 0 50%, 0 0)';
+    }, 7 * 1000);
+
+    setTimeout(function () {
+        deathStarDiv.style.clipPath = 'polygon(50% 0, 50% 50%, 0 0, 0 0, 0 0, 0 0)';
+    }, 8 * 1000);
+
+    setTimeout(function () {
+        deathStarDiv.style.clipPath = 'polygon(50% 0, 50% 50%, 50% 0, 50% 0, 50% 0, 50% 0)';
+    }, 9 * 1000);
     return deathStarDiv;
 };
