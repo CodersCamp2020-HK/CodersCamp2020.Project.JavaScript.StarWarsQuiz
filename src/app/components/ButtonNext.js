9887179ad300281ee5810ebf9c902ad13f18d891
Ania-Em <aniaarchi@gmail.com>
2021-01-04 22:50:36 +0100

    Generic button

---
+++ src/app/components/ButtonNext.js
@@ -0,0 +1,26 @@
+//import {} from {}
+
+class ButtonNext {
+    constructor(text="NEXT", onClickFunction) {
+        this._element = document.createElement('button');
+        this._element.textContent = text;
+        this._element.classList.add('button');
+        this._element.onclick = onClickFunction;
+    }
+
+    get element() {
+        return this._element; 
+    }
+
+    activate() {
+        this._element.classList.add('active');
+        this._element.disabled = false;
+    }
+
+    deactivate() {
+        this._element.classList.remove('active');
+        this._element.disabled = true;
+    }
+}
+
+export { Button };
