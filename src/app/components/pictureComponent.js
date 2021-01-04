b384de94f18eb5df08c4a92bc842862895e82063
Ania-Em <aniaarchi@gmail.com>
2021-01-04 20:43:41 +0100

    Test added

--- src/app/components/pictureComponent.js
+++ src/app/components/pictureComponent.js
@@ -1,10 +1,12 @@
 // import {} from '';
 
-let generatePictureQuestion = function (questionCategory = 'people', pictureNumber = 1) {
+const pictureClassName = 'picture-question';
+
+const generatePictureQuestion = (questionCategory = 'people', pictureNumber = 1) => {
     const picture = document.createElement('img');
-    picture.src = `./src/static/assets/img/modes/${questionCategory}/${pictureNumber}.jpg`;
-    picture.className = 'picture-question';
+    picture.src = `/static/assets/img/modes/${questionCategory}/${pictureNumber}.jpg`;
+    picture.className = pictureClassName;
     return picture;
 };
 
-export { generatePictureQuestion };
+export { generatePictureQuestion, pictureClassName };
