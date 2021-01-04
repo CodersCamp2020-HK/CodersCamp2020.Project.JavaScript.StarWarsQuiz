471ad203655b76ecb53ad2520ab230d3c4137dca
Ania-Em <aniaarchi@gmail.com>
2021-01-04 23:47:29 +0100

    Tests added

--- src/app/components/logo.js
+++ src/app/components/logo.js
@@ -1,8 +1,8 @@
-const LogoPicture = function () {
+const logoPicture = () => {
     const logoContainer = document.createElement('img');
     logoContainer.src = './src/static/assets/ui/StarWarsLogo.png';
     logoContainer.className = 'Logo';
     return logoContainer;
 };
 
-export { LogoPicture };
+export { logoPicture };
