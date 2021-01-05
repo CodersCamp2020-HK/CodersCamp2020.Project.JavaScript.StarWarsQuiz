f0012373a40b4b0d0f87ca034a291449ea8c69fc
bat098 <mateusz.baciak@gmail.com>
2021-01-05 21:59:47 +0100

    question-set v4.0

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,11 @@
 import {RandData} from './infrastructure/RandData'
-import {Data} from './infrastructure/fetchApi'
-import {PrepareData} from './infrastructure/processData'
+// import {Data} from './infrastructure/fetchApi'
+// import {PrepareData} from './infrastructure/processData'
 
 export const App = async () => {
-    const data = new Data();
-    const prepareData = new PrepareData();
-    const categoryArr = await data.getPeopleJsonData().then(res => prepareData.preprocessData(res) )
-    const randData = new RandData()
-    randData.randData(categoryArr, 2).then(res => console.log(res))
+    // const data = new Data();
+    // const prepareData = new PrepareData();
+    // const categoryArr = await data.getPeopleJsonData().then(res => prepareData.preprocessData(res) )
+    // const randData = new RandData()
+    // randData.randData(categoryArr, 2).then(res => console.log(res))
 };
