1bf352c10fcc35e8647b42a62c24c7cd5046a5f8
Weronika Dziedzic <wera02@gmail.com>
2021-01-05 14:35:53 +0100

    Add component to App.

--- src/app/App.js
+++ src/app/App.js
@@ -1,1 +1,7 @@
-export const App = () => {};
+import {QuestionScoreView} from './components/QuestionScoreView';
+ 
+export const App = () => {
+    const container = document.querySelector('#swquiz-app')
+    const questionScoreView = new QuestionScoreView()
+    container.appendChild(questionScoreView)
+};
