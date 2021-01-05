0013bac1223705e760f430b33f8c434639ee4ca1
Ania-Em <aniaarchi@gmail.com>
2021-01-05 16:32:43 +0100

    Appgit add.

--- src/app/App.js
+++ src/app/App.js
@@ -1,1 +1,6 @@
-export const App = () => {};
+import { ButtonNext } from '.components/ButtonNext';
+
+export const App = () => {
+    const nextButton = new ButtonNext('NEXT', nowaZagadka);
+    document.querySelector("body").appendChild(nextButton.element);
+};
