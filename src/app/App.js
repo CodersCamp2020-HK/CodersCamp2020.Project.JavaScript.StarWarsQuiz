98567dd5e9896ac965780dc38160d0685405dcee
Ania-Em <aniaarchi@gmail.com>
2021-01-05 16:33:48 +0100

    Call function in App

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,6 @@
 import { ButtonNext } from '.components/ButtonNext';
 
 export const App = () => {
-    const nextButton = new ButtonNext('NEXT', nowaZagadka);
-    document.querySelector("body").appendChild(nextButton.element);
+    const nextButton = new ButtonNext('NEXT', toTheNextQuestion);
+    document.querySelector('body').appendChild(nextButton.element);
 };
