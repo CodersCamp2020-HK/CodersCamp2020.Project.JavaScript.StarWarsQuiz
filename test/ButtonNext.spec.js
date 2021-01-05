960db274ec48d42a391efb55febd7a575fe81a13
Ania-Em <aniaarchi@gmail.com>
2021-01-05 15:05:49 +0100

    Test added

---
+++ test/ButtonNext.spec.js
@@ -0,0 +1,30 @@
+import { ButtonNext } from '../src/app/components/ButtonNext';
+
+describe('generate button', () => {
+    const buttonClassName = 'button';
+    const alertFunction = () => new Error('Button clicked');
+    const sample = new ButtonNext('Blablabla', alertFunction);
+
+    it('should create button element', () => {
+        expect(sample.element.tagName).toEqual('BUTTON');
+    });
+
+    it('should assign correct class name', () => {
+        expect(sample.element.className).toEqual(buttonClassName);
+    });
+
+    it('should call a function', () => {
+        expect(sample.element.onclick).toEqual(alertFunction);
+    });
+
+    it('should disable button on deactivate', () => {
+        sample.deactivate();
+        expect(sample.element.disabled).toEqual(true);
+    });
+
+    it('should ensable button on activate', () => {
+        sample.activate();
+        expect(sample.element.disabled).toEqual(false);
+    });
+
+});
