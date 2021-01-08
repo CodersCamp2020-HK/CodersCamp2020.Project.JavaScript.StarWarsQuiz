1725ded9c2141f24c2480abceaa2b5f6c368ee75
Mateusz Król <matek8952@gmail.com>
2021-01-08 16:04:25 +0100

    Controller invoked in App.js

--- src/app/App.js
+++ src/app/App.js
@@ -1,1 +1,8 @@
-export const App = () => {};
+import { QuizDataController } from './infrastructure/QuizDataController';
+
+export const App = () => {
+    QuizDataController.createController('people').then((controller) => {
+        console.log(controller.answers);
+        console.log(controller.category);
+    });
+};
