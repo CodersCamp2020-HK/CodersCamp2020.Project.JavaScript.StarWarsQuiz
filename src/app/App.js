d446ec7bf8303286f3e8a79d9b7f3fc6a4a7c556
bat098 <mateusz.baciak@gmail.com>
2021-01-08 17:20:03 +0100

    remove unnecessary class

--- src/app/App.js
+++ src/app/App.js
@@ -1,7 +1,1 @@
-import { PointsCounter } from './components/PointsCounter';
-
-export const App = () => {
-    const container = document.querySelector('#swquiz-app');
-    const pointsCounter = new PointsCounter();
-    container.appendChild(pointsCounter.createPointsCounter());
-};
+export const App = () => {};
--- src/app/components/PointsCounter.js
+++ src/app/components/PointsCounter.js
@@ -1,15 +1,17 @@
-export class PointsCounter {
-    createPointsCounter(pkt = 0) {
+export function pointsCounter(pkt) {
+    if (pkt) {
         const wrapper = document.createElement('div');
         const desc = document.createElement('div');
         const points = document.createElement('div');
-        desc.innerHTML = 'Points';
-        points.innerHTML = pkt;
+        desc.appendChild(document.createTextNode('Points'));
+        points.appendChild(document.createTextNode(pkt));
         wrapper.className = 'pointsCounter_wrapper';
         desc.className = 'pointsCounter_desc';
         points.className = 'pointsCounter_points';
         wrapper.appendChild(desc);
         wrapper.appendChild(points);
         return wrapper;
+    } else {
+        throw new Error('No argument pass to function.');
     }
 }
