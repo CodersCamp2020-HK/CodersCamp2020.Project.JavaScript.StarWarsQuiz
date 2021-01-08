312aa8721108933a319cc12727465fd9cac74b6d
Weronika Dziedzic <wera02@gmail.com>
2021-01-08 19:15:48 +0100

    QuestionScoreView syntax improved

--- src/app/components/QuestionScoreView.js
+++ src/app/components/QuestionScoreView.js
@@ -1,39 +1,42 @@
 export class QuestionScoreComponent {
     constructor(numOfAllAns) {
-      this.numOfAllAns = numOfAllAns;
-      this.numofCorrectAns = 0
-      this.numofIncorrectAns = 0
+        this.numOfAllAns = numOfAllAns;
+        this.numofCorrectAns = 0;
+        this.numofIncorrectAns = 0;
+    }
 
-      this.view = document.createElement("div")
-      this.numofCorrectAnsView = document.createElement("span")
-      this.numofCorrectAnsView.style.color = "green"
-      this.numofCorrectAnsView.textContent = this.numofCorrectAns.toString()
-      this.view.appendChild(this.numofCorrectAnsView);
+    generateViewDiv() {
+        this.view = document.createElement('div');
+        this.numofCorrectAnsView = document.createElement('span');
+        this.numofCorrectAnsView.className = 'display-num-of-good';
+        this.numofCorrectAnsView.style.color = 'green';
+        this.numofCorrectAnsView.textContent = this.numofCorrectAns.toString();
+        this.view.appendChild(this.numofCorrectAnsView);
 
-      document.createTextNode("/") 
+        this.view.appendChild(document.createTextNode('/'));
 
-      this.numofIncorrectAnsView = document.createElement("span")
-      this.numofIncorrectAnsView.style.color = "red"
-      this.numofIncorrectAnsView.textContent = this.numofIncorrectAns.toString()
-      this.view.appendChild(this.numofIncorrectAnsView);
+        this.numofIncorrectAnsView = document.createElement('span');
+        this.numofIncorrectAnsView.style.color = 'red';
+        this.numofIncorrectAnsView.textContent = this.numofIncorrectAns.toString();
+        this.view.appendChild(this.numofIncorrectAnsView);
 
-      document.createTextNode("/") 
+        this.view.appendChild(document.createTextNode('/'));
 
-      this.numOfAllAnsView = document.createElement("span")
-      this.numOfAllAnsView.style.color = "white"
-      this.numOfAllAnsView.textContent = this.numOfAllAns.toString()
-      this.view.appendChild(this.numOfAllAnsView);
+        this.numOfAllAnsView = document.createElement('span');
+        this.numOfAllAnsView.style.color = 'white';
+        this.numOfAllAnsView.textContent = this.numOfAllAns.toString();
+        this.view.appendChild(this.numOfAllAnsView);
+
+        return this.view;
     }
 
     setCorrectAns(value) {
-      this.numofCorrectAns = value
-      this.numofCorrectAnsView.textContent = this.numofCorrectAns.toString()
+        this.numofCorrectAns = value;
+        this.numofCorrectAnsView.textContent = this.numofCorrectAns.toString();
     }
 
     setIncorrectAns(value) {
-      this.numofIncorrectAns = value
-      this.numofIncorrectAnsView.textContent = this.numofCorrectAns.toString()
+        this.numofIncorrectAns = value;
+        this.numofIncorrectAnsView.textContent = this.numofCorrectAns.toString();
     }
-  }
-
-
+}
