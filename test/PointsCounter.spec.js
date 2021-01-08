6d90548a5ffee6977dc3e8e738dd05bccf211012
bat098 <mateusz.baciak@gmail.com>
2021-01-08 17:31:20 +0100

    fix test

--- test/PointsCounter.spec.js
+++ test/PointsCounter.spec.js
@@ -1,8 +1,7 @@
-import { PointsCounter } from '../src/app/components/PointsCounter';
+import { pointsCounter } from '../src/app/components/PointsCounter';
 import { queries } from '@testing-library/dom';
 
 describe('Given: PointsCounter object named pointsCounter', () => {
-    const pointsCounter = new PointsCounter();
     test.each([
         [6, '6'],
         [101, '101'],
@@ -10,7 +9,7 @@ describe('Given: PointsCounter object named pointsCounter', () => {
         `When: Invokes on it method createPointsCounter with (pkt = %s)
          Then: It should creates div element with (text = 'Points %s')`,
         async (param, expected) => {
-            const element = pointsCounter.createPointsCounter(param);
+            const element = pointsCounter(param);
             expect(element.tagName).toEqual('DIV');
             await queries.findByText(element, 'Points');
             await queries.findByText(element, expected);
