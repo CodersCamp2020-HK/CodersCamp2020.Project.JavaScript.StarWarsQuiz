f86c49cc6086afed3507720548568e501a8e9454
Weronika Dziedzic <wera02@gmail.com>
2021-01-08 20:34:32 +0100

    Clear the App.js file

--- src/app/App.js
+++ src/app/App.js
@@ -1,8 +1,1 @@
-import {QuestionScoreView} from './components/QuestionScoreView';
- 
-export const App = () => {
-    const container = document.querySelector('#swquiz-app')
-    const questionScoreView = new QuestionScoreView(valueAll)
-    container.appendChild(questionScoreView.setCorrectAns(valueGood))
-    container.appendChild(questionScoreView.setIncorrectAns(valueBad))
-};
+export const App = () => {};
