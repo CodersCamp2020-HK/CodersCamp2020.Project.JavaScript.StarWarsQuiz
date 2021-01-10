0687958f8a92391c104fcad6033461fa94c0c479
Hubert Kawałek <hubert.kawalek.dev@gmail.com>
2021-01-10 17:46:11 +0100

    Add SWAPI Mock using msw

--- src/app/App.js
+++ src/app/App.js
@@ -1,1 +1,10 @@
-export const App = () => {};
+import { Data } from './infrastructure/fetchApi';
+
+export const App = () => {
+    const dataProvider = new Data();
+    [
+        dataProvider.getPeopleJsonData,
+        dataProvider.getStarshipsJsonData,
+        dataProvider.getVehiclesJsonData,
+    ].forEach((fn) => fn().then((data) => console.log(data)));
+};
--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 import 'regenerator-runtime/runtime'; //async/await with Parcel
 import { App } from './app/App';
+import { setupWorker } from 'msw';
+import { swapiHandlers } from '../test/mocks/SWAPI/SWAPIHandlers';
 
-App({});
+const NODE_ENV = process.env.NODE_ENV || 'development';
+
+const main = async () => {
+    if (NODE_ENV === 'development') {
+        const worker = setupWorker(...swapiHandlers);
+        worker.printHandlers();
+        await worker.start();
+    }
+
+    App({});
+};
+
+main();
