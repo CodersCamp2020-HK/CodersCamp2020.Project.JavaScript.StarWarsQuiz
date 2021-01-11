1b9973fedcb3509884b6bf7ea75ff5033c96ae35
Mateusz Król <matek8952@gmail.com>
2021-01-11 19:18:06 +0100

    Quiz psettings added as aprams for main

--- src/app/App.js
+++ src/app/App.js
@@ -2,7 +2,7 @@ import { QuizGame } from './domain/quiz-game';
 
 export const App = () => {
     const obj = new QuizGame();
-    obj.main().then((div) => {
+    obj.main({ numberOfQuestions: 5, category: 'people', timeInSeconds: 20 }).then((div) => {
         document.querySelector('#swquiz-app').appendChild(div);
     });
 };
--- src/app/domain/quiz-game.js
+++ src/app/domain/quiz-game.js
@@ -14,152 +14,153 @@ export class QuizGame {
         this.questionIndex = 0;
     }
 
-    async main() {
-        const mainDiv = await createController({ category: 'people', numberOfQuestions: 10 }).then((controller) => {
-            const pointsController = new QuestionScoreComponent(controller.numberOfQuestions);
-            const pointsDiv = pointsController.generateViewDiv();
-            const mainDiv = document.createElement('div');
-            const timeInSeconds = 10;
-            const logo = logoPicture();
-            const question = new DisplayQuestion(controller.category);
-            let pointsWrapper = pointsCounter(0);
-            let points = pointsWrapper.querySelector('.pointsCounter_points').textContent;
-            const questionPicture = generatePictureQuestion(
-                controller.category,
-                controller.correctAnswer[this.questionIndex].index,
-            );
-            let currentSelected = null;
+    async main({ category, numberOfQuestions, timeInSeconds }) {
+        const mainDiv = await createController({ category: category, numberOfQuestions: numberOfQuestions }).then(
+            (controller) => {
+                const pointsController = new QuestionScoreComponent(controller.numberOfQuestions);
+                const pointsDiv = pointsController.generateViewDiv();
+                const mainDiv = document.createElement('div');
+                const logo = logoPicture();
+                const question = new DisplayQuestion(controller.category);
+                let pointsWrapper = pointsCounter(0);
+                let points = pointsWrapper.querySelector('.pointsCounter_points').textContent;
+                const questionPicture = generatePictureQuestion(
+                    controller.category,
+                    controller.correctAnswer[this.questionIndex].index,
+                );
+                let currentSelected = null;
 
-            const answer1 = generateAnswerButton({
-                text: controller.answers[this.questionIndex][0].name,
-                onClick: () => {
-                    if (currentSelected) {
-                        currentSelected.unselect();
-                        currentSelected = answer1;
-                        currentSelected.select();
+                const answer1 = generateAnswerButton({
+                    text: controller.answers[this.questionIndex][0].name,
+                    onClick: () => {
+                        if (currentSelected) {
+                            currentSelected.unselect();
+                            currentSelected = answer1;
+                            currentSelected.select();
+                        } else {
+                            currentSelected = answer1;
+                            currentSelected.select();
+                        }
+                    },
+                });
+                const answer2 = generateAnswerButton({
+                    text: controller.answers[this.questionIndex][1].name,
+                    onClick: () => {
+                        if (currentSelected) {
+                            currentSelected.unselect();
+                            currentSelected = answer2;
+                            currentSelected.select();
+                        } else {
+                            currentSelected = answer2;
+                            currentSelected.select();
+                        }
+                    },
+                });
+                const answer3 = generateAnswerButton({
+                    text: controller.answers[this.questionIndex][2].name,
+                    onClick: () => {
+                        if (currentSelected) {
+                            currentSelected.unselect();
+                            currentSelected = answer3;
+                            currentSelected.select();
+                        } else {
+                            currentSelected = answer3;
+                            currentSelected.select();
+                        }
+                    },
+                });
+                const answer4 = generateAnswerButton({
+                    text: controller.answers[this.questionIndex][3].name,
+                    onClick: () => {
+                        if (currentSelected) {
+                            currentSelected.unselect();
+                            currentSelected = answer4;
+                            currentSelected.select();
+                        } else {
+                            currentSelected = answer4;
+                            currentSelected.select();
+                        }
+                    },
+                });
+
+                const answersArray = [answer1, answer2, answer3, answer4];
+
+                const timerAndDeathStarDiv = document.createElement('div');
+                const answerDiv = document.createElement('div');
+                answerDiv.className = 'answers-wrapper';
+                const deathStarDiv = deathStar({ sec: timeInSeconds });
+                const timer = generateTimer({
+                    timeleftInSeconds: timeInSeconds,
+                    onTimerEnd: () => {
+                        console.log('Koniec czasu');
+                        return;
+                    },
+                });
+                timerAndDeathStarDiv.appendChild(deathStarDiv.element);
+                timerAndDeathStarDiv.appendChild(timer);
+
+                const buttonNext = new Button('NEXT', () => {
+                    // Poprawna odpowiedz
+                    if (currentSelected.text() == controller.correctAnswer[this.questionIndex].name) {
+                        currentSelected.markAsCorrect();
+                        pointsController.numofCorrectAns++;
+                        pointsController.setCorrectAns(pointsController.numofCorrectAns);
+                        points = parseInt(points);
+                        points += 10;
+                        pointsWrapper.querySelector('.pointsCounter_points').textContent = points;
                     } else {
-                        currentSelected = answer1;
-                        currentSelected.select();
+                        // Niepoprawna odpowiedz
+                        const correctAnswer = answersArray.filter(
+                            (answer) => answer.text() == controller.correctAnswer[this.questionIndex].name,
+                        )[0];
+                        correctAnswer.markAsCorrect();
+                        currentSelected.markAsWrong();
+                        pointsController.numofIncorrectAns++;
+                        pointsController.setIncorrectAns(pointsController.numofIncorrectAns);
                     }
-                },
-            });
-            const answer2 = generateAnswerButton({
-                text: controller.answers[this.questionIndex][1].name,
-                onClick: () => {
-                    if (currentSelected) {
-                        currentSelected.unselect();
-                        currentSelected = answer2;
-                        currentSelected.select();
-                    } else {
-                        currentSelected = answer2;
-                        currentSelected.select();
+                    controller.currentQuestionNumber++;
+                    this.questionIndex++;
+                    if (controller.currentQuestionNumber > controller.numberOfQuestions) {
+                        console.log('Koniec pytań');
+                        return;
                     }
-                },
-            });
-            const answer3 = generateAnswerButton({
-                text: controller.answers[this.questionIndex][2].name,
-                onClick: () => {
-                    if (currentSelected) {
-                        currentSelected.unselect();
-                        currentSelected = answer3;
-                        currentSelected.select();
-                    } else {
-                        currentSelected = answer3;
-                        currentSelected.select();
-                    }
-                },
-            });
-            const answer4 = generateAnswerButton({
-                text: controller.answers[this.questionIndex][3].name,
-                onClick: () => {
-                    if (currentSelected) {
-                        currentSelected.unselect();
-                        currentSelected = answer4;
-                        currentSelected.select();
-                    } else {
-                        currentSelected = answer4;
-                        currentSelected.select();
-                    }
-                },
-            });
 
-            const answersArray = [answer1, answer2, answer3, answer4];
+                    setTimeout(() => {
+                        questionPicture.src = `/static/assets/img/modes/${controller.category}/${
+                            controller.correctAnswer[this.questionIndex].index
+                        }.jpg`;
+                        answersArray.forEach((but) => {
+                            but.clearClasses();
+                        });
+                        answersArray.forEach((answer, index) => {
+                            answer.setText(controller.answers[this.questionIndex][index].name);
+                        });
+                    }, 2000);
+                });
 
-            const timerAndDeathStarDiv = document.createElement('div');
-            const answerDiv = document.createElement('div');
-            answerDiv.className = 'answers-wrapper';
-            const deathStarDiv = deathStar({ sec: timeInSeconds });
-            const timer = generateTimer({
-                timeleftInSeconds: timeInSeconds,
-                onTimerEnd: () => {
-                    console.log('Koniec czasu');
-                    return;
-                },
-            });
-            timerAndDeathStarDiv.appendChild(deathStarDiv.element);
-            timerAndDeathStarDiv.appendChild(timer);
+                buttonNext.element.classList.add('button-next');
 
-            const buttonNext = new Button('NEXT', () => {
-                // Poprawna odpowiedz
-                if (currentSelected.text() == controller.correctAnswer[this.questionIndex].name) {
-                    currentSelected.markAsCorrect();
-                    pointsController.numofCorrectAns++;
-                    pointsController.setCorrectAns(pointsController.numofCorrectAns);
-                    points = parseInt(points);
-                    points += 10;
-                    pointsWrapper.querySelector('.pointsCounter_points').textContent = points;
-                } else {
-                    // Niepoprawna odpowiedz
-                    const correctAnswer = answersArray.filter(
-                        (answer) => answer.text() == controller.correctAnswer[this.questionIndex].name,
-                    )[0];
-                    correctAnswer.markAsCorrect();
-                    currentSelected.markAsWrong();
-                    pointsController.numofIncorrectAns++;
-                    pointsController.setIncorrectAns(pointsController.numofIncorrectAns);
-                }
-                controller.currentQuestionNumber++;
-                this.questionIndex++;
-                if (controller.currentQuestionNumber > controller.numberOfQuestions) {
-                    console.log('Koniec pytań');
-                    return;
-                }
+                answerDiv.appendChild(answer1.element);
+                answerDiv.appendChild(answer2.element);
+                answerDiv.appendChild(answer3.element);
+                answerDiv.appendChild(answer4.element);
 
-                setTimeout(() => {
-                    questionPicture.src = `/static/assets/img/modes/${controller.category}/${
-                        controller.correctAnswer[this.questionIndex].index
-                    }.jpg`;
-                    answersArray.forEach((but) => {
-                        but.clearClasses();
-                    });
-                    answersArray.forEach((answer, index) => {
-                        answer.setText(controller.answers[this.questionIndex][index].name);
-                    });
-                }, 2000);
-            });
+                mainDiv.appendChild(logo);
+                mainDiv.appendChild(question.generateQuestion({ questionNumber: controller.currentQuestionNumber }));
+                mainDiv.appendChild(pointsDiv);
+                mainDiv.appendChild(questionPicture);
+                mainDiv.appendChild(answerDiv);
+                mainDiv.appendChild(timerAndDeathStarDiv);
+                mainDiv.appendChild(pointsWrapper);
+                mainDiv.appendChild(buttonNext.element);
 
-            buttonNext.element.classList.add('button-next');
+                mainDiv.classList = 'quiz-game-wrapper';
 
-            answerDiv.appendChild(answer1.element);
-            answerDiv.appendChild(answer2.element);
-            answerDiv.appendChild(answer3.element);
-            answerDiv.appendChild(answer4.element);
+                buttonNext.element.onclick;
 
-            mainDiv.appendChild(logo);
-            mainDiv.appendChild(question.generateQuestion({ questionNumber: controller.currentQuestionNumber }));
-            mainDiv.appendChild(pointsDiv);
-            mainDiv.appendChild(questionPicture);
-            mainDiv.appendChild(answerDiv);
-            mainDiv.appendChild(timerAndDeathStarDiv);
-            mainDiv.appendChild(pointsWrapper);
-            mainDiv.appendChild(buttonNext.element);
-
-            mainDiv.classList = 'quiz-game-wrapper';
-
-            buttonNext.element.onclick;
-
-            return mainDiv;
-        });
+                return mainDiv;
+            },
+        );
         return mainDiv;
     }
 }
