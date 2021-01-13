eecda6caee8995dc3d57b2bfb9f3772e9432d676
bat098 <mateusz.baciak@gmail.com>
2021-01-13 22:37:49 +0100

    username v1.0

---
+++ src/app/components/UserName.js
@@ -0,0 +1,14 @@
+import '../../styles/UserName';
+export const InputName = () => {
+    const wrapperElement = document.createElement('div');
+    const userElement = document.createElement('p');
+    const inputElement = document.createElement('input');
+    inputElement.placeholder = 'Write your name here';
+    wrapperElement.appendChild(userElement);
+    wrapperElement.appendChild(inputElement);
+    wrapperElement.className = 'inputName_wrapper';
+    userElement.className = 'inputName_user';
+    inputElement.className = 'inputName_input';
+    userElement.appendChild(document.createTextNode('NAME:'));
+    return wrapperElement;
+};
