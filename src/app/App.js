be110844178ac849da826657a7beab3f15da92ba
Mateusz Król <matek8952@gmail.com>
2021-01-13 21:40:03 +0100

    Components not acreating html

--- src/app/App.js
+++ src/app/App.js
@@ -1,8 +1,13 @@
-import { QuizGame } from './domain/quiz-game';
+// import { QuizGame } from './domain/quiz-game';
+import { generateRulesButton, generateRulesModal } from './components/Rules';
 
 export const App = () => {
-    const obj = new QuizGame();
-    obj.main({ numberOfQuestions: 5, category: 'people', timeInSeconds: 20 }).then((div) => {
-        document.querySelector('#swquiz-app').appendChild(div);
-    });
+    // const obj = new QuizGame();
+    // obj.main({ numberOfQuestions: 5, category: 'people', timeInSeconds: 20 }).then((div) => {
+    //     document.querySelector('#swquiz-app').appendChild(div);
+    // });
+    const modal = generateRulesModal();
+    const rulesButton = generateRulesButton({ text: 'Rules', modalDiv: modal });
+    document.querySelector('#swquiz-app').appendChild(modal);
+    document.querySelector('#swquiz-app').appendChild(rulesButton);
 };
---
+++ src/app/components/Rules.js
@@ -0,0 +1,43 @@
+import { Button } from './Button';
+
+export const generateRulesButton = ({ text, modalDiv }) => {
+    const button = new Button(text);
+    button.className = 'mainpage-button-rules';
+    button.element.onclick = () => {
+        modalDiv.showModal();
+    };
+
+    return button;
+};
+
+const generateCloseRulesButton = ({ modalDiv }) => {
+    const button = new Button('x');
+    button.element.classList = 'rules-modal-close';
+    button.element.onclick = () => {
+        modalDiv.close();
+    };
+
+    return button;
+};
+
+export const generateRulesModal = () => {
+    const rulesText = `You have one minute (1m) to answer 10 questions. During the game on each question you need to select who from Star Wars is showed on the left (Jar Jar Binks right now) from available options, or select that the character is none of above.`;
+
+    const dialog = document.createElement('dialog');
+    dialog.className = 'rules-modal';
+
+    const header = document.createElement('h5');
+    header.className = 'rules-modal-header';
+
+    const closeButton = generateCloseRulesButton({ modalDiv: dialog });
+
+    const p = document.createElement('p');
+    p.className = 'rules-modal-text';
+    p.textContent = rulesText;
+
+    dialog.appendChild(header);
+    dialog.appendChild(closeButton);
+    dialog.appendChild(p);
+
+    return dialog;
+};
