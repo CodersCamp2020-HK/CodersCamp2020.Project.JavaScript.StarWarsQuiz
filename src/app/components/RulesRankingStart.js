5f4f689154d80d028eab1ac25351869ed0ca47e3
Mateusz Król <matek8952@gmail.com>
2021-01-14 11:13:08 +0100

    All buttons working

---
+++ src/app/components/RulesRankingStart.js
@@ -0,0 +1,38 @@
+import { Button } from './Button';
+
+const generateRulesButton = ({ modalDiv }) => {
+    const button = new Button('Rules');
+    button.element.className = 'mainpage-button-rules';
+    button.element.onclick = () => {
+        modalDiv.showModal();
+    };
+
+    return button.element;
+};
+
+const generateRankingButton = ({ onClick }) => {
+    const button = new Button('Ranking', onClick);
+    button.element.className = 'mainpage-button-ranking';
+
+    return button.element;
+};
+
+const generateStartButton = ({ onClick }) => {
+    const button = new Button('Play', onClick);
+    button.element.className = 'mainpage-button-start';
+
+    return button.element;
+};
+
+export const generateRulesRankingStartButtons = ({ modalDiv, rankingOnClick, startOnClick }) => {
+    const buttonsDiv = document.createElement('div');
+    buttonsDiv.className = 'mainpage-wrapper-rules-ranking-start';
+
+    const rulesButton = generateRulesButton({ modalDiv: modalDiv });
+    const rankingButton = generateRankingButton({ onClick: rankingOnClick });
+    const startButton = generateStartButton({ onClick: startOnClick });
+
+    buttonsDiv.append(rulesButton, rankingButton, startButton);
+
+    return buttonsDiv;
+};
