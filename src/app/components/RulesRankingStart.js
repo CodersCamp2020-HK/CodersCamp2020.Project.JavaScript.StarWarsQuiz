cd120a46b86c869ec75fc97aa5dd3423e2450395
Mateusz Król <matek8952@gmail.com>
2021-01-14 18:32:26 +0100

    Deleted wrapper for buttons

--- src/app/components/RulesRankingStart.js
+++ src/app/components/RulesRankingStart.js
@@ -1,8 +1,8 @@
 import { Button } from './Button';
 
-const generateRulesButton = ({ modalDiv }) => {
+export const generateRulesButton = ({ modalDiv }) => {
     const button = new Button('Rules');
-    button.element.className = 'mainpage-button mainpage-button-rules';
+    button.element.className = 'mainpage__button mainpage__button--rules';
     button.element.onclick = () => {
         modalDiv.showModal();
     };
@@ -10,29 +10,16 @@ const generateRulesButton = ({ modalDiv }) => {
     return button.element;
 };
 
-const generateRankingButton = ({ onClick }) => {
+export const generateRankingButton = ({ onClick }) => {
     const button = new Button('Ranking', onClick);
-    button.element.className = 'mainpage-button mainpage-button-ranking';
+    button.element.className = 'mainpage__button mainpage__button--ranking';
 
     return button.element;
 };
 
-const generateStartButton = ({ onClick }) => {
+export const generateStartButton = ({ onClick }) => {
     const button = new Button('Play', onClick);
-    button.element.className = 'mainpage-button mainpage-button-start';
+    button.element.className = 'mainpage__button mainpage__button--start';
 
     return button.element;
-};
-
-export const generateRulesRankingStartButtons = ({ modalDiv, rankingOnClick, startOnClick }) => {
-    const buttonsDiv = document.createElement('div');
-    buttonsDiv.className = 'mainpage-wrapper-rules-ranking-start';
-
-    const rulesButton = generateRulesButton({ modalDiv: modalDiv });
-    const rankingButton = generateRankingButton({ onClick: rankingOnClick });
-    const startButton = generateStartButton({ onClick: startOnClick });
-
-    buttonsDiv.append(rulesButton, rankingButton, startButton);
-
-    return buttonsDiv;
 };
