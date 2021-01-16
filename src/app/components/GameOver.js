27c168bd67448258accc868c36eafaa09f2d34a2
Weronika Dziedzic <wera02@gmail.com>
2021-01-16 17:07:45 +0100

    Add getter element

--- src/app/components/GameOver.js
+++ src/app/components/GameOver.js
@@ -1,9 +1,13 @@
-class gameOver {
+class GameOver {
     constructor(text) {
         this._element = document.createElement('div');
         this._element.textContent = text;
         this._element.className = 'gameOverDiv';
     }
+
+    get elemnt() {
+        return this._element;
+    }
 }
 
-export { gameOver };
+export { GameOver };
