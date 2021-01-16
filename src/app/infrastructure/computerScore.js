23e99f776b700d62d8348549fdaa2df4553e110f
bat098 <mateusz.baciak@gmail.com>
2021-01-16 15:30:04 +0100

    add component refactor

--- src/app/infrastructure/computerScore.js
+++ src/app/infrastructure/computerScore.js
@@ -8,5 +8,5 @@ export const computerScore = (level, numberOfQuestions) => {
     if (numberOfQuestions <= 0) throw new Error('NumberOfQuestions should to be greater than 0');
     if (!(level in getRandomNumberGeneratorByLevel)) throw new Error('Argument level should to be a number 1,2 or 3');
     const randomNumber = getRandomNumberGeneratorByLevel[level]();
-    return Math.floor(numberOfQuestions * (randomNumber / 100)) * 5;
+    return Math.floor(numberOfQuestions * (randomNumber / 100)) * 10;
 };
