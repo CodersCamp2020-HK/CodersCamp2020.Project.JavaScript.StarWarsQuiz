23fa017e409fb3dabe9890653cd4d7dab361b21c
bat098 <mateusz.baciak@gmail.com>
2021-01-16 16:46:53 +0100

    refactor component

--- src/app/components/ComputerScore.js
+++ src/app/components/ComputerScore.js
@@ -1,34 +1,19 @@
-export function ComputerScore(score) {
-    if (score >= 0) {
-        const wrapperElement = document.createElement('div');
-        const descElement = document.createElement('div');
-        const scoreElement = document.createElement('div');
-        const pointsWordElement = document.createElement('div');
-        wrapperElement.className = 'computerScore_wrapper';
-        descElement.className = 'computerScore_desc';
-        scoreElement.className = 'computerScore_score';
-        pointsWordElement.className = 'computerScore_pointsWord';
-
-        const getData = async () => {
-            const randomPage = Math.floor(Math.random() * 8) + 1;
-            const randomPerson = Math.floor(Math.random() * 11);
-            await fetch(`https://swapi.dev/api/people/?page=${randomPage}`)
-                .then((res) => res.json())
-                .then((data) =>
-                    descElement.appendChild(
-                        document.createTextNode(`${data.results[randomPerson].name} gained in total:`),
-                    ),
-                );
-        };
-        getData();
-
-        scoreElement.appendChild(document.createTextNode(`${score}`));
-        pointsWordElement.appendChild(document.createTextNode(`Points`));
-        wrapperElement.appendChild(descElement);
-        wrapperElement.appendChild(scoreElement);
-        wrapperElement.appendChild(pointsWordElement);
-        return wrapperElement;
-    } else {
-        throw new Error('Argument score should be grater or equal of 0');
-    }
+export function ComputerScore(score, name) {
+    if (typeof score !== 'number') throw new Error(`score should to be greater than 0`);
+    if (typeof name !== 'string') throw new Error(`name should to be string type`);
+    const wrapperElement = document.createElement('div');
+    const descElement = document.createElement('div');
+    const scoreElement = document.createElement('div');
+    const pointsWordElement = document.createElement('div');
+    wrapperElement.className = 'computerScore_wrapper';
+    descElement.className = 'computerScore_desc';
+    scoreElement.className = 'computerScore_score';
+    pointsWordElement.className = 'computerScore_pointsWord';
+    descElement.appendChild(document.createTextNode(`${name} gained in total:`));
+    scoreElement.appendChild(document.createTextNode(`${score}`));
+    pointsWordElement.appendChild(document.createTextNode(`Points`));
+    wrapperElement.appendChild(descElement);
+    wrapperElement.appendChild(scoreElement);
+    wrapperElement.appendChild(pointsWordElement);
+    return wrapperElement;
 }
