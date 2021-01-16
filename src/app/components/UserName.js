98fd9988b270060d0c02941e78272e6ed53eb544
bat098 <mateusz.baciak@gmail.com>
2021-01-16 15:03:08 +0100

    add validation to input name

--- src/app/components/UserName.js
+++ src/app/components/UserName.js
@@ -10,5 +10,13 @@ export const InputName = () => {
     userElement.className = 'inputName_user';
     inputElement.className = 'inputName_input';
     userElement.appendChild(document.createTextNode('NAME:'));
+    inputElement.addEventListener('blur', (e) => {
+        if (e.target.value === '') {
+            e.target.classList.add('inputName_input-empty');
+        }
+    });
+    inputElement.addEventListener('focus', (e) => {
+        e.target.classList.remove('inputName_input-empty');
+    });
     return wrapperElement;
 };
