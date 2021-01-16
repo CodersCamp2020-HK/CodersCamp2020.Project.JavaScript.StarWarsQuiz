0e3d6ff9249acf26dcbc0b3ee9ce3a56a885c5a8
bat098 <mateusz.baciak@gmail.com>
2021-01-16 15:20:19 +0100

    component refactor 2

--- src/app/infrastructure/computerScore.js
+++ src/app/infrastructure/computerScore.js
@@ -1,25 +1,12 @@
+const getRandomNumberGeneratorByLevel = {
+    1: () => Math.floor(Math.random() * 34), //0-33
+    2: () => 34 + Math.floor(Math.random() * 33), //34-66
+    3: () => 67 + Math.floor(Math.random() * 34), //67-100
+};
+
 export const computerScore = (level, numberOfQuestions) => {
-    if (numberOfQuestions > 0) {
-        let score;
-        let randomNumber;
-        switch (level) {
-            case 1:
-                randomNumber = Math.floor(Math.random() * 34); //0-33
-                score = Math.floor(numberOfQuestions * (randomNumber / 100)) * 10;
-                break;
-            case 2:
-                randomNumber = 34 + Math.floor(Math.random() * 33); //34-66
-                score = Math.floor(numberOfQuestions * (randomNumber / 100)) * 10;
-                break;
-            case 3:
-                randomNumber = 67 + Math.floor(Math.random() * 34); //67-100
-                score = Math.floor(numberOfQuestions * (randomNumber / 100)) * 10;
-                break;
-            default:
-                throw new Error('Argument level should to be a number 1,2 or 3');
-        }
-        return score;
-    } else {
-        throw new Error('NumberOfQuestions should to be greater than 0');
-    }
+    if (numberOfQuestions <= 0) throw new Error('NumberOfQuestions should to be greater than 0');
+    if (!(level in getRandomNumberGeneratorByLevel)) throw new Error('Argument level should to be a number 1,2 or 3');
+    const randomNumber = getRandomNumberGeneratorByLevel[level]();
+    return Math.floor(numberOfQuestions * (randomNumber / 100)) * 5;
 };
