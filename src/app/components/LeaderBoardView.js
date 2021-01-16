df4e211a7932fd955e731a12ac61c3dda062ace0
Haivex <64418079+Haivex@users.noreply.github.com>
2021-01-16 17:15:11 +0100

    Added Leaderboard (#92)
    * Created leaderboard
    
    * Added classes to leaderboard
    
    * Remove unused imports
    
    * Added styles
    
    Co-authored-by: Haivex <demozial@gmail.com>

---
+++ src/app/components/LeaderBoardView.js
@@ -0,0 +1,35 @@
+export function generateLeaderBoard() {
+    let leaderBoardDiv = document.createElement('div');
+    leaderBoardDiv.classList.add('leaderboard-container');
+    let rankingDiv = document.createElement('div');
+
+    leaderBoardDiv.appendChild(rankingDiv);
+
+    let view = {
+        element: leaderBoardDiv,
+        buttons: null,
+        selectedRanking: rankingDiv,
+        selectRanking: (rankingView) => {
+            let rankingClone = rankingView.element.cloneNode(true);
+            view.selectedRanking.replaceWith(rankingClone);
+            view.selectedRanking = rankingClone;
+        },
+        addLevelsButtons: (buttonsViews) => {
+            let categoriesDiv = document.createElement('div');
+            categoriesDiv.classList.add('categories-container');
+            let categoriesName = document.createElement('span');
+            categoriesName.classList.add('categories-name');
+            categoriesName.textContent = 'CATEGORIES';
+            categoriesDiv.appendChild(categoriesName);
+
+            buttonsViews.forEach((difficultLevelButton) => {
+                categoriesDiv.appendChild(difficultLevelButton.element);
+            });
+
+            view.buttons = buttonsViews;
+
+            leaderBoardDiv.insertBefore(categoriesDiv, rankingDiv);
+        },
+    };
+    return view;
+}
