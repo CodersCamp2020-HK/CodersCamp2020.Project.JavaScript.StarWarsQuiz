873796fdf16132ba5cc280f99e9b764a2f9caa72
Mateusz Król <matek8952@gmail.com>
2021-01-17 21:44:46 +0100

    Cache added, pictures not loading fixed

--- src/app/infrastructure/createController.js
+++ src/app/infrastructure/createController.js
@@ -3,22 +3,46 @@ import { PrepareData } from './processData';
 import { RandData } from './RandData';
 import { QuizDataController } from '../domain/QuizDataController';
 
+let isPeopleCached = false;
+let isVehiclesCached = false;
+let isStarshipsCached = false;
+let peopleData;
+let vehiclesData;
+let starshipsData;
+
 export const createController = async ({ category, numberOfQuestions }) => {
     const processedData = new PrepareData();
     const randData = new RandData();
     const jsonData = new Data();
 
     let data;
-
     switch (category) {
         case 'people':
-            data = await jsonData.getPeopleJsonData();
+            if (isPeopleCached) {
+                data = peopleData;
+            } else {
+                data = await jsonData.getPeopleJsonData();
+                peopleData = data;
+                isPeopleCached = true;
+            }
             break;
         case 'vehicles':
-            data = await jsonData.getStarshipsJsonData();
+            if (isVehiclesCached) {
+                data = vehiclesData;
+            } else {
+                data = await jsonData.getVehiclesJsonData();
+                vehiclesData = data;
+                isVehiclesCached = true;
+            }
             break;
         case 'starships':
-            data = await jsonData.getStarshipsJsonData();
+            if (isStarshipsCached) {
+                data = starshipsData;
+            } else {
+                data = await jsonData.getStarshipsJsonData();
+                starshipsData = data;
+                isStarshipsCached = true;
+            }
             break;
         default:
             throw new Error('Category should be people, vehicles or starships');
